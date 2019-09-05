9b545dbd00059d2975fe00626924a3b1aa2f9225
Carlos Santos <csantos2@uncc.edu>
2019-09-05 01:03:06 -0400

    Added ATMOps test file, and also outline on ATMOps file.

--- src/main/java/com/revature/service/ATMOperations.java
+++ src/main/java/com/revature/service/ATMOperations.java
@@ -4,6 +4,18 @@ public class ATMOperations {
 	
 	static double currBalance;
 	
+	public static void verification() {
+		/* 
+		 * 1. goto repository
+		 * 2. use BufferReader to read names from .txt file for time being
+		 * 2a. or use PostgreSQL database
+		 * 3. Find using binarySearch
+		 * 4. return User object if found.
+		 * 4a. if not, return to controller.
+		 * 
+		 */
+	}
+	
 	public static double depositMoney(double balance) {
 		return currBalance += balance;
 	}
