1875435c4751abfaf172b06710b97be0ab02af5a
Carlos Santos <csantos2@uncc.edu>
2019-09-06 11:32:51 -0400

    added find and update methods in TempDB

--- src/main/java/com/revature/repository/TemporaryDB.java
+++ src/main/java/com/revature/repository/TemporaryDB.java
@@ -7,12 +7,25 @@ public class TemporaryDB {
 	private static List<UserBA> database;
 	
 	TemporaryDB() {
-		database = new LinkedList<UserBA>();
+		database = new ArrayList<UserBA>();
 		addUserBA("Tammy","EEEFFFF",100000.00);
 		addUserBA("Evan","AAAAA",70000.00);
 	}
 	
 	public void addUserBA(String name, String password, double amount) {
 		database.add(new UserBA(name,password,amount));
+	}
+	
+	public UserBA getUserBA(String name) throws NullPointerException {
+		for (UserBA a : database) {
+			if (a.getName().equals(name))
+				return a;
+		}
+		return null;
+	}
+	
+	public void updateUserBA(UserBA user) {
+		UserBA temp = getUserBA(user.getName());
+		temp.setName(user.getName());
 	}
 }
