18fb1a3355763fdab2c6d52f4bddd2875547b29a
Carlos Santos <csantos2@uncc.edu>
2019-09-06 12:11:22 -0400

    Changed test file name

--- src/test/java/com/revature/service/TemporaryDBTest.java
+++ src/test/java/com/revature/service/TemporaryDBTest.java
@@ -7,8 +7,7 @@ import org.junit.Test;
 import com.revature.model.UserBA;
 import com.revature.repository.TemporaryDB;
 
-public class ATMOperationsTest {
-
+public class TemporaryDBTest {
 	TemporaryDB ins = new TemporaryDB();
 	@Test
 	public void addNewUserToBank() {
@@ -20,7 +19,7 @@ public class ATMOperationsTest {
 	public void searchCurrentUser() {
 		UserBA example = new UserBA("Tammy","EEEFFFF",100000.00);
 		// assert that the example object is the same as the object retrieved
-		// using the getUserBA account.
+		// using the getUserBA method.
 	}
 
 }
