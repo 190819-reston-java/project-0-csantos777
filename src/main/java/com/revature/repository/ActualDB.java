528396eceacf99269dece673b5f5b6c9e67c3644
Carlos Santos <csantos2@uncc.edu>
2019-09-10 15:32:47 -0400

    Added actualDB file

---
+++ src/main/java/com/revature/repository/ActualDB.java
@@ -0,0 +1,18 @@
+package com.revature.repository;
+
+import java.sql.*;
+/*
+ * This is only a draft.
+ */
+public class ActualDB {
+	public Connection connection;
+	/* 
+	 * static {
+	 * connection sets to postgresql in aws webserver.
+	 * }
+	 * 
+	 * then methods.
+	 * 
+	 * 
+	 */
+}
